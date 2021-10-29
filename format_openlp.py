7509e0aeced16ce5ce0a9f54204be8f5631c005e
David Ogirala <davidoraj@gmail.com>
2021-10-29 09:51:56 -0700

    Save song to xml, for importing to OpenLP (v 2.4.6)

--- format_openlp.py
+++ format_openlp.py
@@ -1,4 +1,7 @@
 import re
+import string
+from collections import OrderedDict
+import xml.dom.minidom
 
 default_lines_per_slide = 4
 section_lookup = {'V': 'Verse', 'C': 'Chorus', 'P': 'Pre-chorus', 'D': 'Bridge', 'T': 'Other', 'O': 'Other'}
@@ -6,6 +9,11 @@ sections = {'Verse', 'Chorus', 'Pre-chorus', 'Bridge', 'Tag', 'Instrumental', 'B
 ignore_section = {'BREAK'}
 sections_regex = '|'.join(['^' + section + '[0-9 ]*:$' for section in sections])
 repeat_section_regex = '|'.join(['^' + section + '[0-9 ]*' for section in sections])
+
+# Variables used for creating song xml
+verse_parts = list(string.ascii_lowercase)  # Creates ['a', 'b', 'c', ..., 'z']
+line_delim = '<br/>'
+section_delim = '<br/><br/>'
 
 # c-chorus, n-verse, p-prechorus, b-bridge, c2
 
@@ -52,7 +60,7 @@ def get_song_annotated(one, two, lines_per_slide, title):
         section, id = is_section(line)
         rsection, rid = is_repeat_Section(line)
         if section:
-            if section[0] == 'I':
+            if section[0] == 'I':  # Instrumental
                 sid = 'O' + str(id)
             else:
                 sid = section[0] + str(id)
@@ -91,7 +99,11 @@ def get_song_annotated(one, two, lines_per_slide, title):
                 i = i + 1
                 break
 
-    return {'sections': sections, 'order': order, 'lines_per_slide': lines_per_slide, 'title': title}
+    return {'sections': sections,
+            'order': order,
+            'verse_order': ' '.join(order),
+            'lines_per_slide': lines_per_slide,
+            'title': title}
 
 
 # Parses the songs from text (list of lines)
@@ -110,17 +122,32 @@ def get_song_objects(song_lines_list):
         else:
             lines_per_slide = default_lines_per_slide
 
+        # Parse song to object (based on annotations)
         song = get_song_annotated(one, two, lines_per_slide, title)
+
+        # Create slides (English + Telugu combined)
+        create_song_slide_deck(song)
+
         songs.append(song)
 
     return songs
 
 
-def append_section_slides(label, deck, section, lines_per_slide=default_lines_per_slide):
+def append_section_lyrics_xml(id, verse_part_counter, slides):
+    counter = verse_part_counter.get(id, -1) + 1
+    verse_part_counter[id] = counter
+    vid = id.lower() + verse_parts[counter]
+    return vid, slides[1:]
+
+
+def append_section_slides(label, id, deck, section, lines_per_slide=default_lines_per_slide):
     # Init vars
     n = len(section)
     line_count = n if n < lines_per_slide else lines_per_slide
     i_last_slide = n if n % lines_per_slide == 0 else n - (n % lines_per_slide)  # index for last slide
+
+    verse_part_counter = {}  # verse index (with alphabet counter, v1a, v1b etc)
+    section_lyrics_dict = OrderedDict()
 
     # list of lines in a slide (added to given deck)
 
@@ -130,6 +157,9 @@ def append_section_slides(label, deck, section, lines_per_slide=default_lines_pe
         slides.append(label)
         for j in range(line_count):
             slides.append(section[j + i])
+
+        vid, slide_lines = append_section_lyrics_xml(id, verse_part_counter, slides)
+        section_lyrics_dict[vid] = slide_lines
         deck.append(slides)
 
     # Last slide for the section
@@ -138,9 +168,12 @@ def append_section_slides(label, deck, section, lines_per_slide=default_lines_pe
         slides.append(label)
         for i in range(i_last_slide, n):
             slides.append(section[i])
+
+        vid, slide_lines = append_section_lyrics_xml(id, verse_part_counter, slides)
+        section_lyrics_dict[vid] = slide_lines
         deck.append(slides)
 
-    return
+    return section_lyrics_dict
 
 
 def merge_section_slides(main_deck, temp_deck, i):
@@ -151,7 +184,7 @@ def merge_section_slides(main_deck, temp_deck, i):
         main_deck.append(temp_deck[i][j])
 
 
-def merge_decks(deck1, deck2, order):
+def merge_decks(deck1, deck2):
     # deck1 and deck2 lengths should be the same at this point
     deck = []
     n = len(deck1)
@@ -165,15 +198,31 @@ def merge_decks(deck1, deck2, order):
             deck.append("")  # empty line
             merge_section_slides(deck, deck2, i)
 
-    return deck, ' '.join(order)
+    return deck
+
+
+# Takes two lists of tuples, and returns an OrderedDict of named lyrics (c1a->..., c1b->..., v1a->... etc.)
+def merge_lyrics_xml(dict1, dict2):
+    lyrics_xml = OrderedDict()
+
+    for id in dict1.keys():
+        xml_string = line_delim.join(dict1[id])
+        if dict2:
+            xml_string = xml_string + section_delim + line_delim.join(dict2[id])
+        lyrics_xml[id] = xml_string
+
+    return lyrics_xml
 
 
 # Given an annotated song (sections/order)
 # it returns the slides for the song (English & Telugu)
-def get_song_slide_deck(song):
+def create_song_slide_deck(song):
     section_map = set()
     deck1 = []  # english
     deck2 = []  # telugu
+    lyrics1_xml_dict = OrderedDict()
+    lyrics2_xml_dict = OrderedDict()
+
     for o in song['order']:
         one = o + '_one'
         two = o + '_two'
@@ -198,12 +247,18 @@ def get_song_slide_deck(song):
         else:
             section_map.add(label)
 
-        append_section_slides(label, deck1, section1, song['lines_per_slide'])
+        # Convert a section (with n lines) to k slides, and put in a dict with vid (verse id) as key
+        lyrics1_xml_dict.update(append_section_slides(label, o, deck1, section1, song['lines_per_slide']))
         if section2:
-            append_section_slides(label, deck2, section2, song['lines_per_slide'])
+            lyrics2_xml_dict.update(append_section_slides(label, o, deck2, section2, song['lines_per_slide']))
 
-    # Merge decks into a a single deck
-    return merge_decks(deck1, deck2, song['order'])
+    # Merge decks into a a single deck (song text in "edit-all")
+    song['deck'] = merge_decks(deck1, deck2)
+
+    # Merge lyrics xml for song import
+    song['lyrics_xml'] = merge_lyrics_xml(lyrics1_xml_dict, lyrics2_xml_dict)
+
+    return
 
 
 # Returns list of tuples (englist_lines_list, optional_telugu_lines_list)
@@ -234,25 +289,36 @@ def get_song_lines(lines):
 
 
 def get_xml(tag, text, props=''):
-    return '<{tag}{props}>{text}</{tag}>'.format(tag=tag, text=text, props=props)
+    return '<{tag} {props}>{text}</{tag}>'.format(tag=tag, text=text, props=props)
 
 
-def save_to_xml(song, deck, order):
-    start = """
-    <?xml version='1.0' encoding='UTF-8'?>
-    <song xmlns="http://openlyrics.info/namespace/2009/song" version="0.8" createdIn="OpenLP 2.4.6" modifiedIn="OpenLP 2.4.6">
-    """
+def save_to_xml(song):
+    start = """<?xml version='1.0' encoding='UTF-8'?>
+        <song xmlns="http://openlyrics.info/namespace/2009/song" 
+        version="0.8" createdIn="OpenLP 2.4.6" modifiedIn="OpenLP 2.4.6">"""
+
     properties_text = \
         get_xml('titles', get_xml('title', song['title'])) + \
         get_xml('authors', get_xml('author', 'Unknown')) + \
-        get_xml('verseOrder', order.lower())
+        get_xml('verseOrder', song['verse_order'].lower())
     properties = get_xml('properties', properties_text)
 
-    lyrics_text = ''
-    lyrics = get_xml('lyrics', lyrics_text)
+    # Create a list of verses with name=verse_id
+    # song['lyrics_xml'] is an ordered dict with key=verse_id, value=[slide-lyrics]
+    lyrics_xml_list = []
+    for vid in song['lyrics_xml']:
+        verse_lines = get_xml('lines', ''.join(song['lyrics_xml'][vid]))
+        lyrics_xml_list.append(get_xml('verse', verse_lines, 'name="{}"'.format(vid)))
     end = "</song>"
 
-    return start + properties + lyrics + end
+    # Put combine xml content into one string
+    song_xml = start + properties + get_xml('lyrics', ''.join(lyrics_xml_list)) + end
+
+    # Write to file
+    with open(song['title'] + '.xml', 'w') as xml_file:
+        # TODO: Find a way to pretty print without disturbing verse lines text
+        # xml_file.write(xml.dom.minidom.parseString(song_xml).toprettyxml())
+        xml_file.write(song_xml)
 
 
 def main():
@@ -264,14 +330,14 @@ def main():
 
     # Create slides
     for song in songs:
-        deck, order = get_song_slide_deck(song)
 
         # Export to xml
-        save_to_xml(song, deck, order)
+        save_to_xml(song)
 
+        # Print text
         print("----------------------------------")
-        print(' '.join(order.split()))
-        for line in deck:
+        print(song['verse_order'])
+        for line in song['deck']:
             print(line)
 
 
